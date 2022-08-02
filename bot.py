eafce07a8f12843411744c7e0ffa54634f95ded7
iggyscode <iggydeveloper@protonmail.com>
2022-08-02 17:26:16 -0400

    fixed bug where button wasnt being clicked

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-from threading import Thread
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -14,11 +15,23 @@ class Bot:
 
     def get_internet_speed(self):
         self.driver.get("https://www.speedtest.net/")
-        start_btn = self.driver.find_element(By.CLASS_NAME, "start-text")
+        
+        start_btn = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a')
         start_btn.click()
+
+        #TODO extract upload and download values 
+        # up_speed = self.driver.find_element(By.CLASS_NAME, "download-speed")
+        # down_speed = self.driver.find_element(By.CLASS_NAME, "upload-speed")
+
+        # print(up_speed.text)
+        # print(down_speed.text)
         #TODO: find a way to prevent driver from closing until speedtest is finsihed
-        self.driver.implicitly_wait(20)
-        self.driver.quit()
+        # down_speed = 0
+        # up_speed = 0
+
+        #self.driver implicitly waits for 20 seconds   
+        
+        # self.driver.quit()
 
 
 
